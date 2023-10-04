018de95bbde67d8b81eab12a963d8ef609a31ac6
Viper <Viper@LAPTOP-A7GJT87A>
2023-10-03 22:20:48 -0500

    Add project files.

---
+++ ReverseString/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ReverseString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is a string reversal tool!\n\n Enter a string of characters and press enter. \n");
+
+            string userInput = Console.ReadLine();
+            Console.WriteLine("You entered: " + userInput+"\n");
+
+            string reversed = "";
+            int length = userInput.Length;
+            for (int letterCount = length; letterCount > 0; letterCount--)
+            {
+                reversed += userInput[letterCount -1];
+            }
+            Console.WriteLine('"' + userInput + '"' + " reversed is: " + reversed);
+            if (reversed == userInput)
+            {
+                Console.WriteLine(reversed + " is a palindrome!");
+            }
+        }
+    }
+}
